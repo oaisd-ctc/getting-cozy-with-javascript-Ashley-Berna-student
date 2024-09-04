9b7c1cb761a243c3d6f01d49471f85e4637b7fe9
Ashley <144158076+Ashley-Berna-student@users.noreply.github.com>
2024-09-04 18:33:06 +0000

    added another file

---
+++ ArrowFunctions&MoreInteractivity/scripts/apps.js
@@ -0,0 +1,20 @@
+document.getElementById("btn-hide").addEventListener("click", wordDissapears);
+document.getElementById("btn-show").addEventListener("click", wordAppears);
+document.getElementById("btn-change-color").addEventListener("click", changeTextColor);
+
+function wordDissapears()
+{
+    document.getElementById("p2").style.visibility = "hidden";
+}
+
+function wordAppears()
+{
+    document.getElementById("p2").style.visibility = "visible";
+}
+
+function changeTextColor()
+{
+    var input = document.getElementById("txt-color").value;
+    document.getElementById("p2").style.color = input;
+}
+
