40e368ac15e50ec00c175c275c996063fcbd50fe
Ashley <144158076+Ashley-Berna-student@users.noreply.github.com>
2024-09-05 18:49:51 +0000

    still working on arrow stuff

--- ArrowFunctions&MoreInteractivity/scripts/apps.js
+++ ArrowFunctions&MoreInteractivity/scripts/apps.js
@@ -18,3 +18,12 @@ function changeTextColor()
     document.getElementById("p3").style.background = input;
 }
 
+const changeShape = document.getElementById("changeShape");
+const shape = document.getElementById("shape");
+
+shape.addEventListener('change', () => {
+    if (changeShape == '#circle')
+    {
+        document.getElementById('shape').style.borderRadius = 500;
+    }
+})
